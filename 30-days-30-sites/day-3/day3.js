c8fcca5ad783ee79b8f53bf2ab7a773820f36ff0
ismael garcia <leamsigc@hotmail.com>
2017-03-26 09:51:24 -0400

    finish the day 3 site

--- 30-days-30-sites/day-3/day3.js
+++ 30-days-30-sites/day-3/day3.js
@@ -11,7 +11,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("slides");
+ var slides = document.getElementsByClassName("slides");
     var dots = document.getElementsByClassName("dot");
     if (n > slides.length) {
         slideIndex = 1
@@ -22,9 +22,19 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i > dots.length; i++) {
+    /*for (i = 0; i > dots.length; i++) {
         dots[i].className = dots[i].className.replace("active", "");
-    }
+    }*/
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+   /* dots[slideIndex - 1].className += " active";*/
 }
+/*GOOGLE MAP TO THE SITE */
+function myMap(){
+    var mapCanvas=document.getElementById('map');//get the map container
+    var mapOptions={
+        center:new google.maps.LatLng(51.5, -0.2),
+        zoom:10
+
+    };
+    var map= new google.maps.Map(mapCanvas,mapOptions);
+};
